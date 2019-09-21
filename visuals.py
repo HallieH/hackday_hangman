35e4a456891da1a66640b6ae8c109db38ef51941
unknown <brycejones503@gmail.com>
2019-09-21 16:25:24 -0500

    added the hang station and the hangman

--- visuals.py
+++ visuals.py
@@ -1,5 +1,18 @@
 import pygame
 
 def visual(screen):
-    pygame.draw.circle(screen, pygame.Color(225,0,0), (400,400), 100)
+    pygame.draw.line(screen, pygame.Color(153, 76, 0), (50, 100), (50, 450), 10)#main pole
+    pygame.draw.line(screen,  pygame.Color(153, 76, 0), (0, 450),(200,450), 50)#base
+    pygame.draw.line(screen, pygame.Color(153, 76, 0), (50, 104), (300, 104), 10)#outreach of pole
+    pygame.draw.line(screen, pygame.Color(153, 76, 0), (297, 100), (297, 155), 10)#pole holding rope
+    pygame.draw.line(screen, pygame.Color(227, 227, 227), (297, 155), (297, 175), 1)#rope to man
+    pygame.draw.circle(screen, pygame.Color(225,225,225), (297,205), 30)#head
+    pygame.draw.line(screen, pygame.Color(225,225,225), (297,230),(297,340), 1)#body
+    pygame.draw.line(screen, pygame.Color(225,225,225), (297,340),(334,430), 1)#left leg
+    pygame.draw.line(screen, pygame.Color(225,225,225), (297,340),(260,430), 1)#right leg
+    pygame.draw.line(screen, pygame.Color(225,225,225), (297,255),(277,330), 1)#Left arm
+    pygame.draw.line(screen, pygame.Color(225,225,225), (297,255),(317,330), 1)#right arm
+
+
     pygame.display.update()
+    
